f72de65fa84327a8cba4674a83ae054517ac05ab
jhuva <jdzambranoh@unac.edu.pe>
2021-04-06 20:02:15 -0500

    Segundo commit

---
+++ src/app/dbz/agregar-personajes/agregar-personajes.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar-personajes',
+  templateUrl: './agregar-personajes.component.html',
+})
+export class AgregarPersonajesComponent {
+
+  @Input() nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  constructor ( private dbzService: DbzService ) {}
+  
+  // cambiarNombre( event: any ) {
+  //   console.log( event.target.value )
+  // }
+  
+  //@Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+  agregar() {
+    if ( this.nuevo.nombre.trim().length === 0 ) {
+      return;
+    }
+    //this.onNuevoPersonaje.emit( this.nuevo )
+    this.dbzService.agregarPersonaje( this.nuevo );
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+
+  }
+
+}
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+@Injectable()
+export class DbzService {
+
+    private _personajes: Personaje[] = [
+        {
+          nombre: 'Goku',
+          poder: 20000
+        },
+        {
+          nombre: 'Vegeta',
+          poder: 15000
+        }
+      ];
+
+    constructor() {
+        console.log('Servicio inicializado')
+    }
+
+    get personajes(): Personaje[] {
+        return [...this._personajes];
+    }
+
+    agregarPersonaje( personaje: Personaje ) {
+        this._personajes.push( personaje );    
+    }
+
+}
